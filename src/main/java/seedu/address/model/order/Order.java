2afffe36515c76e53368d8c2bb57c9de0df3b685
amad-person <e0072565@u.nus.edu>
2018-03-11 16:57:22 +0800

    Added Orders to application Model

---
+++ src/main/java/seedu/address/model/order/Order.java
@@ -0,0 +1,8 @@
+package seedu.address.model.order;
+
+/**
+ * Represents an Order in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Order {
+}
