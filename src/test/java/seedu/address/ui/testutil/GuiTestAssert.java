984be625341907a1cd3a1b45951ce9ff4ed25687
AJZ1995 <jingzhe321@gmail.com>
2018-03-14 11:18:29 +0800

    fix more errors

--- src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
+++ src/test/java/seedu/address/ui/testutil/GuiTestAssert.java
@@ -11,7 +11,6 @@ import guitests.guihandles.PersonCardHandle;
 import guitests.guihandles.PersonListPanelHandle;
 import guitests.guihandles.ResultDisplayHandle;
 import seedu.address.model.person.Person;
-import seedu.address.ui.PersonCard;
 
 /**
  * A set of assertion methods useful for writing GUI tests.
@@ -27,7 +26,8 @@ public class GuiTestAssert {
         assertEquals(expectedCard.getName(), actualCard.getName());
         assertEquals(expectedCard.getTags(), actualCard.getTags());
 
-        expectedCard.getTags().forEach(tag -> assertEquals(expectedCard.getTagStyleClasses(tag), actualCard.getTagStyleClasses(tag)));
+        expectedCard.getTags().forEach(tag ->
+                assertEquals(expectedCard.getTagStyleClasses(tag), actualCard.getTagStyleClasses(tag)));
     }
 
     /**
