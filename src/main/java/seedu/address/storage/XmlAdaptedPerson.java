76579375474d8adbad8f245eebb6d354f56bbd52
SuxianAlicia <suxianalicia@gmail.com>
2018-03-15 17:57:04 +0800

    Modified Storage component

--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -14,6 +14,8 @@ import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.tag.Group;
+import seedu.address.model.tag.Preference;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -33,7 +35,8 @@ public class XmlAdaptedPerson {
     private String address;
 
     @XmlElement
-    private List<XmlAdaptedTag> tagged = new ArrayList<>();
+    private List<XmlAdaptedGroup> groups = new ArrayList<>();
+    private List<XmlAdaptedPreference> preferences = new ArrayList<>();
 
     /**
      * Constructs an XmlAdaptedPerson.
@@ -44,13 +47,17 @@ public class XmlAdaptedPerson {
     /**
      * Constructs an {@code XmlAdaptedPerson} with the given person details.
      */
-    public XmlAdaptedPerson(String name, String phone, String email, String address, List<XmlAdaptedTag> tagged) {
+    public XmlAdaptedPerson(String name, String phone, String email, String address, List<XmlAdaptedGroup> groups,
+                            List<XmlAdaptedPreference> preferences) {
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
-        if (tagged != null) {
-            this.tagged = new ArrayList<>(tagged);
+        if (groups != null) {
+            this.groups = new ArrayList<>(groups);
+        }
+        if (preferences != null) {
+            this.preferences = new ArrayList<>(preferences);
         }
     }
 
@@ -64,9 +71,14 @@ public class XmlAdaptedPerson {
         phone = source.getPhone().value;
         email = source.getEmail().value;
         address = source.getAddress().value;
-        tagged = new ArrayList<>();
-        for (Tag tag : source.getTags()) {
-            tagged.add(new XmlAdaptedTag(tag));
+        groups = new ArrayList<>();
+        for (Group group : source.getGroupTags()) {
+            groups.add(new XmlAdaptedGroup(group));
+        }
+
+        preferences = new ArrayList<>();
+        for (Preference pref : source.getPreferenceTags()) {
+            preferences.add(new XmlAdaptedPreference(pref));
         }
     }
 
@@ -76,9 +88,14 @@ public class XmlAdaptedPerson {
      * @throws IllegalValueException if there were any data constraints violated in the adapted person
      */
     public Person toModelType() throws IllegalValueException {
-        final List<Tag> personTags = new ArrayList<>();
-        for (XmlAdaptedTag tag : tagged) {
-            personTags.add(tag.toModelType());
+        final List<Group> personGroups = new ArrayList<>();
+        for (XmlAdaptedGroup group : groups) {
+            personGroups.add(group.toModelType());
+        }
+
+        final List<Preference> personPreferences = new ArrayList<>();
+        for (XmlAdaptedPreference pref: preferences) {
+            personPreferences.add(pref.toModelType());
         }
 
         if (this.name == null) {
@@ -113,8 +130,9 @@ public class XmlAdaptedPerson {
         }
         final Address address = new Address(this.address);
 
-        final Set<Tag> tags = new HashSet<>(personTags);
-        return new Person(name, phone, email, address, tags);
+        final Set<Group> groups = new HashSet<>(personGroups);
+        final Set<Preference> preferences = new HashSet<>(personPreferences);
+        return new Person(name, phone, email, address, groups, preferences);
     }
 
     @Override
@@ -132,6 +150,6 @@ public class XmlAdaptedPerson {
                 && Objects.equals(phone, otherPerson.phone)
                 && Objects.equals(email, otherPerson.email)
                 && Objects.equals(address, otherPerson.address)
-                && tagged.equals(otherPerson.tagged);
+                && groups.equals(otherPerson.groups);
     }
 }
