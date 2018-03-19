b73e4272021fc255432f98d5b8795e1cdbbd3b4c
AJZ1995 <jingzhe321@gmail.com>
2018-03-19 17:08:34 +0800

    fix errors

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -40,6 +40,7 @@ public class PersonCard extends UiPart<Region> {
         this.person = person;
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
+
         initTags(person);
     }
 
