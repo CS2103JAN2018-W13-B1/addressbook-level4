af0c98c67a83fdde4309ef8798d2543d6946e691
SuxianAlicia <suxianalicia@gmail.com>
2018-04-09 13:01:33 +0800

    Removed unused import in EditEntryCommandTest.

--- src/test/java/seedu/address/logic/commands/EditEntryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditEntryCommandTest.java
@@ -23,7 +23,6 @@ import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
 import seedu.address.logic.commands.EditEntryCommand.EditEntryDescriptor;
 import seedu.address.model.AddressBook;
-import seedu.address.model.CalendarManager;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
