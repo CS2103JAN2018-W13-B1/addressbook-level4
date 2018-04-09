dd6733b98b2d1788da040a5bc807327521adfa35
SuxianAlicia <suxianalicia@gmail.com>
2018-04-09 12:48:54 +0800

    Edited existing utility classes. Fixed Bug in checking for entry duration in EntryTimeConstraintsUtil.

--- src/main/java/seedu/address/commons/util/EntryTimeConstraintsUtil.java
+++ src/main/java/seedu/address/commons/util/EntryTimeConstraintsUtil.java
@@ -1,7 +1,9 @@
 package seedu.address.commons.util;
 
-import static java.time.temporal.ChronoUnit.MINUTES;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.event.EndDate;
@@ -14,22 +16,25 @@ import seedu.address.model.event.StartTime;
  */
 public class EntryTimeConstraintsUtil {
 
-    public static final String EVENT_DURATION_CONSTRAINTS =
-            "Event must last at least 15 minutes if ending in same day."; //Constraint of CalendarFX entries
-    public static final String STANDARD_START_TIME = "00:00"; //Start Time of event if StartTime not given
+    public static final String ENTRY_DURATION_CONSTRAINTS =
+            "Entry must last at least 15 minutes if ending in same day."; //Constraint of CalendarFX entries
     public static final String START_AND_END_DATE_CONSTRAINTS = "Start Date cannot be later than End Date.";
     public static final String START_AND_END_TIME_CONSTRAINTS =
-            "Start Time cannot be later than End Time if Event ends on same date.";
+            "Start Time cannot be later than End Time if Entry ends on same date.";
 
-    private static final int MINIMAL_DURATION = 15; //Constraint of CalendarFX entries
+    private static final long MINIMAL_DURATION = 15; //Constraint of CalendarFX entries
 
     /**
      * Returns true if duration between start time and end time is less than 15 minutes.
      * This is a constraint that CalendarFX has. Event duration must last at least 15 minutes.
      */
-    private static boolean eventIsShorterThanFifteenMinutes(StartTime startTime, EndTime endTime) {
-        requireAllNonNull(startTime, endTime);
-        if (MINUTES.between(startTime.getLocalTime(), endTime.getLocalTime()) < MINIMAL_DURATION) {
+    private static boolean eventIsShorterThanFifteenMinutes(StartDate startDate, EndDate endDate,
+                                                            StartTime startTime, EndTime endTime) {
+        requireAllNonNull(startDate, endDate, startTime, endTime);
+
+        LocalDateTime startDateAndTime = LocalDateTime.of(startDate.getLocalDate(), startTime.getLocalTime());
+        LocalDateTime endDateAndTime = LocalDateTime.of(endDate.getLocalDate(), endTime.getLocalTime());
+        if (Duration.between(startDateAndTime, endDateAndTime).toMinutes() < MINIMAL_DURATION) {
             return true;
         }
         return false;
@@ -39,7 +44,7 @@ public class EntryTimeConstraintsUtil {
      * Returns true if given start time is later than end time.
      * Start time cannot be later than End time if event ends on the same date.
      */
-    private static boolean startTimeLaterThanEndTime(StartTime startTime, EndTime endTime) {
+    private static boolean startTimeIsLaterThanEndTime(StartTime startTime, EndTime endTime) {
         requireAllNonNull(startTime, endTime);
         return startTime.getLocalTime().isAfter(endTime.getLocalTime());
     }
@@ -48,26 +53,33 @@ public class EntryTimeConstraintsUtil {
      * Returns true if given start date is later than end date.
      * Start Date cannot be later than End Date as it violates the meaning of the terms.
      */
-    private static boolean startDateLaterThanEndDate(StartDate startDate, EndDate endDate) {
+    private static boolean startDateIsLaterThanEndDate(StartDate startDate, EndDate endDate) {
         requireAllNonNull(startDate, endDate);
         return startDate.getLocalDate().isAfter(endDate.getLocalDate());
     }
 
-    public static void checkCalendarEntryTimeConstraints
-            (StartDate startDate, EndDate endDate, StartTime startTime, EndTime endTime) throws IllegalValueException {
+    /**
+     * Checks 3 constraints:
+     * 1. {@code StartDate} must not be after {@code EndDate}.
+     * 2. {@code Start Time} must not be after {@code EndTime} if Calendar Entry ends on same Date.
+     * 3. Duration of entry cannot be less than 15 minutes.
+     */
+    public static void checkCalendarEntryTimeConstraints(
+           StartDate startDate, EndDate endDate, StartTime startTime, EndTime endTime) throws IllegalValueException {
 
-        if (startDateLaterThanEndDate(startDate, endDate)) {
+        if (startDateIsLaterThanEndDate(startDate, endDate)) {
             throw new IllegalValueException(START_AND_END_DATE_CONSTRAINTS);
         }
 
-        if (startDate.toString().equals(endDate.toString())) {
-            if (startTimeLaterThanEndTime(startTime, endTime)) {
-                throw new IllegalValueException(START_AND_END_TIME_CONSTRAINTS);
-            }
-
-            if (eventIsShorterThanFifteenMinutes(startTime, endTime)) {
-                throw new IllegalValueException(EVENT_DURATION_CONSTRAINTS);
-            }
+        if (startDate.toString().equals(endDate.toString()) && startTimeIsLaterThanEndTime(startTime, endTime)) {
+            throw new IllegalValueException(START_AND_END_TIME_CONSTRAINTS);
         }
+
+        if (eventIsShorterThanFifteenMinutes(startDate, endDate, startTime, endTime)) {
+            throw new IllegalValueException(ENTRY_DURATION_CONSTRAINTS);
+
+        }
+
     }
+
 }
