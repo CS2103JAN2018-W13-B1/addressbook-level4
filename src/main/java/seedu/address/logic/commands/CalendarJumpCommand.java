8c4e75791c356ec91d366a8438fddee883a273de
SuxianAlicia <suxianalicia@gmail.com>
2018-04-11 22:40:07 +0800

    Fixed Example mistake.

--- src/main/java/seedu/address/logic/commands/CalendarJumpCommand.java
+++ src/main/java/seedu/address/logic/commands/CalendarJumpCommand.java
@@ -1,11 +1,10 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TARGET_DATE;
 
 import java.time.LocalDate;
-
-import javax.naming.event.EventContext;
 
 import seedu.address.commons.core.EventsCenter;
 import seedu.address.commons.events.ui.ChangeCalendarDateRequestEvent;
@@ -24,7 +23,7 @@ public class CalendarJumpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Displays given date in Calendar.\n"
             + "Parameters: " + PREFIX_TARGET_DATE + "TARGET_DATE (must be in format DD-MM-YYY)\n"
-            + "Example: " + COMMAND_WORD + " day";
+            + "Example: " + COMMAND_WORD + " 06-06-2018";
 
     public static final String MESSAGE_CALENDAR_JUMP_SUCCESS = "Displayed Date: %1$s in Calendar.";
 
@@ -35,7 +34,7 @@ public class CalendarJumpCommand extends Command {
      * Creates CalendarJumpCommand with specified {@code LocalDate} to display in Calendar.
      */
     public CalendarJumpCommand(LocalDate date, String dateString) {
-        requireNonNull(date);
+        requireAllNonNull(date, dateString);
         this.date = date;
         this.dateString = dateString;
     }
@@ -45,5 +44,13 @@ public class CalendarJumpCommand extends Command {
         requireNonNull(date);
         EventsCenter.getInstance().post(new ChangeCalendarDateRequestEvent(date));
         return new CommandResult(String.format(MESSAGE_CALENDAR_JUMP_SUCCESS, dateString));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof CalendarJumpCommand // instanceof handles nulls
+                && date.equals(((CalendarJumpCommand) other).date)
+                && dateString.equals(((CalendarJumpCommand) other).dateString));
     }
 }
